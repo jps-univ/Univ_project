07dfb781beeec0e642ea85ff0661751ca504e9ac
DuhanMo <devduhanmo@gmail.com>
2020-10-23 17:14:32 +0900

    20201023 Duhan Main Page Font Update

--- src/main/webapp/resources/js/main_page.js
+++ src/main/webapp/resources/js/main_page.js
@@ -3,7 +3,7 @@ $(function(){
         $(this).attr("class","active");
         $(this).siblings('li').attr("class","");
     });
-})
+});
 
 function noticePop(elm){
     window.open("../views/notice_detail.html","","width=900, height=880");
