30d6e555583a28b896af3aee59b271e7e8f29e43
SeokHwan <opiiipoo@gmail.com>
2020-10-26 02:40:09 +0900

    강의게시판 - 페이지 이동(수정필요)

---
+++ src/main/java/com/kh/univ/classBoard/controller/ClassBoardController.java
@@ -0,0 +1,95 @@
+package com.kh.univ.classBoard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClassBoardController {
+
+	
+	/**
+	 * 1. 강의 선택창
+	 * @return
+	 */
+	@RequestMapping("selectClass.do")
+	public String selectClass() {
+		return "classBoard/selectClass";
+	}
+	
+	
+	/**
+	 * 2. 강의계획서 페이지
+	 * @return
+	 */
+	@RequestMapping("classPlan.do")
+	public String classPlan() {
+		return "classBoard/classPlan";
+	}
+
+	
+	
+	/**
+	 * 3. 공지사항 리스트 페이지
+	 * @return
+	 */
+	@RequestMapping("noticeList.do")
+	public String noticeList() {
+		return "classBoard/noticeList";
+	}
+	
+	
+	
+	/**
+	 * 4. 과제 리스트 페이지
+	 * @return
+	 */
+	@RequestMapping("assignmentList.do")
+	public String assignmentList() {
+		return "classBoard/assignmentList";
+	}
+
+	
+	
+	/**
+	 * 4-1. 과제 디테일 페이지
+	 * @return
+	 */
+	@RequestMapping("assignmentDetail.do")
+	public String assignmentDetail() {
+		return "classBoard/assignmentDetail";
+	}
+	
+	
+	/**
+	 * 4-2. 학생 - 과제 제출 페이지
+	 * @return
+	 */
+	@RequestMapping("assignmentEnroll.do")
+	public String assignmentEnroll() {
+		return "classBoard/assignmentEnroll_stu";
+	}
+	
+	
+	/**
+	 * 4-2. 학생 - 과제 제출 페이지
+	 * @return
+	 */
+	@RequestMapping("assignmentWrite.do")
+	public String assignmentWrite() {
+		return "classBoard/assignmentWrite_professor";
+	}
+	
+
+	/**
+	 * 5. 성적 이의 신청 리스트 페이지
+	 * @return
+	 */
+	@RequestMapping("gradeObjectionList.do")
+	public String gradeObjectionList() {
+		return "classsBoard/gradeObjectionList";
+	}
+	
+	
+	
+	
+}
