47bf9d131dda98bbbd8eb7c948bcfd7e672a6490
kim1jo2ho3 <kim1jo2ho3@gmail.com>
2020-10-27 17:29:36 +0900

    commit

---
+++ src/main/java/com/kh/univ/lecture/controller/LectureController.java
@@ -0,0 +1,13 @@
+package com.kh.univ.lecture.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LectureController {
+
+	@RequestMapping("lecture_evaluation.do")
+	public String lectureEvaluation() {
+		return "admin/lecture_evaluation";
+	}
+}
