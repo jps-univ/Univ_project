b49a8ef0391a91c15f7a8ee29a7698d0b536e942
kim1jo2ho3 <kim1jo2ho3@gmail.com>
2020-10-27 22:53:47 +0900

    d

--- src/main/java/com/kh/univ/lecture/controller/LectureController.java
+++ src/main/java/com/kh/univ/lecture/controller/LectureController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LectureController {
 
+	/** 1. 강의 평가 하기( 학생)
+	 * @return
+	 */
 	@RequestMapping("lecture_evaluation.do")
 	public String lectureEvaluation() {
 		return "lectureManagement/lecture_evaluation";
