828621e64fe231c0955bfcbaada5a41ba4941840
82107 <82107@DESKTOP-RQPNU3R>
2020-10-28 18:13:58 +0900

    수강생조회 기능 - 혀긔

--- src/main/java/com/kh/univ/stmanagement/controller/StudentManagementController.java
+++ src/main/java/com/kh/univ/stmanagement/controller/StudentManagementController.java
@@ -1,14 +1,29 @@
 package com.kh.univ.stmanagement.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.univ.stmanagement.model.service.StudentManagementService;
+import com.kh.univ.stmanagement.model.vo.StudentManagement;
 
 @Controller
 public class StudentManagementController {
 
+	@Autowired
+	private StudentManagementService smService;
+	
 	@RequestMapping("stList.do")
-	public String StudentView() {
-		return "studentManagement/studentView";
+	public ModelAndView StudentView(ModelAndView mv) {
+		
+		ArrayList<StudentManagement> list = smService.selectList();
+		
+		mv.addObject("list", list);
+		mv.setViewName("studentManagement/studentView");
+		return mv;
 	}
 	
 	@RequestMapping("stListDetail.do")
