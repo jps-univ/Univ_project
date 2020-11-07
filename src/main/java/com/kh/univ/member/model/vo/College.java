7e48ba85c2e5733214ada9ad24d6d41f8c22970e
kim1jo2ho3 <kim1jo2ho3@gmail.com>
2020-11-07 18:11:20 +0900

    join 해서 조회를 하기 위해서 department , college VO 추

---
+++ src/main/java/com/kh/univ/member/model/vo/College.java
@@ -0,0 +1,49 @@
+package com.kh.univ.member.model.vo;
+
+public class College {
+
+	private String collegeCode;		// 단과대학 코드
+	private String collegeName;		// 단과대학명
+	
+	
+	public College() {
+		super();
+	}
+	
+	
+	public College(String collegeCode, String collegeName) {
+		super();
+		this.collegeCode = collegeCode;
+		this.collegeName = collegeName;
+	}
+
+	
+	
+
+	public String getCollegeCode() {
+		return collegeCode;
+	}
+
+
+	public void setCollegeCode(String collegeCode) {
+		this.collegeCode = collegeCode;
+	}
+
+
+	public String getCollegeName() {
+		return collegeName;
+	}
+
+
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "College [collegeCode=" + collegeCode + ", collegeName=" + collegeName + "]";
+	}
+	
+	
+}
