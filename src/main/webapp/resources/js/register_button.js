7d937cda24b972c4dd1a3a9a7655aa9128d0f961
kyoo0802 <kyoo0802@DESKTOP-N3NFFH5>
2020-11-22 23:10:30 +0900

    gg

--- src/main/webapp/resources/js/register_button.js
+++ src/main/webapp/resources/js/register_button.js
@@ -1,5 +1,5 @@
 // 휴학신청
-function button_leave() {
+/*function button_leave() {
     
 	if(confirm("휴학신청하시겠습니까?"))
 	{
@@ -23,15 +23,16 @@ function button_leave() {
 	
 	} else{
 		alert("취소되었습니다.");
-	}
+	}*/
     
 // 복학신청
-}
+
 function button_return() {
     alert("복학이 정상적으로 신청되었습니다.");
+}
     
 // 졸업신청
-}
+
 function button_graduation() {
 	alert("졸업이 정상적으로 신청되었습니다.");
 }
