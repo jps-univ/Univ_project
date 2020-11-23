4834b23d9383795b33b64c547c51eaff7591183a
kyoo0802 <kyoo0802@DESKTOP-N3NFFH5>
2020-11-23 23:23:57 +0900

    11111css

--- src/main/java/com/kh/univ/register/model/vo/Register.java
+++ src/main/java/com/kh/univ/register/model/vo/Register.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 public class Register {
 	private int statusNO;				// 학적코드
 	private int stdId;					// 학번
+	private int stdSemester;                // 학기
 	private String stdStatus;			// 학적 상태
 	private String applicationStatus;	// 신청 상태
 	private Date applicationDate;		// 신청 날짜
@@ -21,11 +22,13 @@ public class Register {
 	}
 
 
-	public Register(int statusNO, int stdId, String stdStatus, String applicationStatus, Date applicationDate,
-			String reasonsLeave, String leavePeriod, Date leaveDate, Date returningDate, Date graduationDate) {
+	public Register(int statusNO, int stdId, int stdSemester, String stdStatus, String applicationStatus,
+			Date applicationDate, String reasonsLeave, String leavePeriod, Date leaveDate, Date returningDate,
+			Date graduationDate) {
 		super();
 		this.statusNO = statusNO;
 		this.stdId = stdId;
+		this.stdSemester = stdSemester;
 		this.stdStatus = stdStatus;
 		this.applicationStatus = applicationStatus;
 		this.applicationDate = applicationDate;
@@ -54,6 +57,16 @@ public class Register {
 
 	public void setStdId(int stdId) {
 		this.stdId = stdId;
+	}
+
+
+	public int getStdSemester() {
+		return stdSemester;
+	}
+
+
+	public void setStdSemester(int stdSemester) {
+		this.stdSemester = stdSemester;
 	}
 
 
@@ -139,11 +152,13 @@ public class Register {
 
 	@Override
 	public String toString() {
-		return "Register [statusNO=" + statusNO + ", stdId=" + stdId + ", stdStatus=" + stdStatus
-				+ ", applicationStatus=" + applicationStatus + ", applicationDate=" + applicationDate
+		return "Register [statusNO=" + statusNO + ", stdId=" + stdId + ", stdSemester=" + stdSemester + ", stdStatus="
+				+ stdStatus + ", applicationStatus=" + applicationStatus + ", applicationDate=" + applicationDate
 				+ ", reasonsLeave=" + reasonsLeave + ", leavePeriod=" + leavePeriod + ", leaveDate=" + leaveDate
 				+ ", returningDate=" + returningDate + ", graduationDate=" + graduationDate + "]";
 	}
+
+
 	
 	
 }
